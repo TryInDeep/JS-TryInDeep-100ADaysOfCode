bec5f26d26a412494973f20816c11035db02d655
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2024-03-22 16:17:42 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = "Tridip"; // 33 , 33abc , null , true/false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// Coverted => String to Number 
+let valueInNumber = Number(score);
+
+//console.log(typeof (valueInNumber));
+//console.log(valueInNumber);
+
+// NaN = Not a Number (NaN special type)
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1 ; false => 0
+// null => 0
+
+
+let isLoggedIn = "Tridip";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Tridip" => true
+
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
