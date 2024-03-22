7c4e95004d0cc037c42573f595c652453cec6207
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2024-03-22 16:54:41 +0000

    did some Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,6 @@
+
+//============================ Conversion ================================/
+
 let score = "Tridip"; // 33 , 33abc , null , true/false
 
 // console.log(typeof score);
@@ -6,8 +9,8 @@ let score = "Tridip"; // 33 , 33abc , null , true/false
 // Coverted => String to Number 
 let valueInNumber = Number(score);
 
-//console.log(typeof (valueInNumber));
-//console.log(valueInNumber);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
 
 // NaN = Not a Number (NaN special type)
 
@@ -24,7 +27,7 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 //console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
-// "" => false
+// "" => 0
 // "Tridip" => true
 
 
@@ -32,5 +35,64 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+
+
+//============================ Operations ===================================//
+
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // power 
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "Hello";
+let str2 = " Tridip";
+let str3 = str1 + str2;
+//console.log(str3);
+
+//Problems 
+
+// console.log("1"+ 2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+ 2 + 2);
+// console.log(1+ 2 + "2");
+
+//console.log(3 + 4 * 5 % 6); // bad habit use brackets 
+
+
+// console.log(+true); // true+ error
+// console.log(+"")
+
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+//gameCounter++; postfix
+//++gameCounter; prefix
+console.log(gameCounter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
