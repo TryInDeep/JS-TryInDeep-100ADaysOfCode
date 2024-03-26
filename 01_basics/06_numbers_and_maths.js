9ce933e190e518f64b023f68e0be95e63c8d0781
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2024-03-26 17:11:33 +0000

    learn about numbers and math

---
+++ 01_basics/06_numbers_and_maths.js
@@ -0,0 +1,168 @@
+const score = 400;
+
+const balance = new Number(100); //special cast
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));// after (.) value
+
+const otherNumber = 1123.8966;
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// constructor
+// : 
+// ƒ Number()
+// toExponential
+// : 
+// ƒ toExponential()
+// toFixed
+// : 
+// ƒ toFixed()
+// toLocaleString
+// : 
+// ƒ toLocaleString()
+// toPrecision
+// : 
+// ƒ toPrecision()
+// toString
+// : 
+// ƒ toString()
+// valueOf
+// : 
+// ƒ valueOf()
+
+//+++++++++++++++++++++++ MATHS ++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4)); // positive but negetive changes to positive
+// console.log(Math.round(4.6));
+
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+
+
+console.log(Math.random()); // output will be in 0 to 1
+console.log((Math.random()*10) + 1); // output will be in 0 to 1
+console.log((Math.floor(Math.random()*10) + 1)); 
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1))+ min);
+
+
+
+
+// abs
+// : 
+// ƒ abs()
+// acos
+// : 
+// ƒ acos()
+// acosh
+// : 
+// ƒ acosh()
+// asin
+// : 
+// ƒ asin()
+// asinh
+// : 
+// ƒ asinh()
+// atan
+// : 
+// ƒ atan()
+// atan2
+// : 
+// ƒ atan2()
+// atanh
+// : 
+// ƒ atanh()
+// cbrt
+// : 
+// ƒ cbrt()
+// ceil
+// : 
+// ƒ ceil()
+// clz32
+// : 
+// ƒ clz32()
+// cos
+// : 
+// ƒ cos()
+// cosh
+// : 
+// ƒ cosh()
+// exp
+// : 
+// ƒ exp()
+// expm1
+// : 
+// ƒ expm1()
+// floor
+// : 
+// ƒ floor()
+// fround
+// : 
+// ƒ fround()
+// hypot
+// : 
+// ƒ hypot()
+// imul
+// : 
+// ƒ imul()
+// log
+// : 
+// ƒ log()
+// log1p
+// : 
+// ƒ log1p()
+// log2
+// : 
+// ƒ log2()
+// log10
+// : 
+// ƒ log10()
+// max
+// : 
+// ƒ max()
+// min
+// : 
+// ƒ min()
+// pow
+// : 
+// ƒ pow()
+// random
+// : 
+// ƒ random()
+// round
+// : 
+// ƒ round()
+// sign
+// : 
+// ƒ sign()
+// sin
+// : 
+// ƒ sin()
+// sinh
+// : 
+// ƒ sinh()
+// sqrt
+// : 
+// ƒ sqrt()
+// tan
+// : 
+// ƒ tan()
+// tanh
+// : 
+// ƒ tanh()
+// trunc
+// : 
+// ƒ trunc()
