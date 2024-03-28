bb8fe16bb0b5847e8e8c25069aa5bb4b68073f56
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2024-03-28 17:13:56 +0000

    using concat over  push method

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,13 @@
+const marvel_heros = ["IronMan","SpiderMan","CaptainAmerica","Thor"];
+const dc_heros =["SuperMan","Flash","BatMan"];
+
+
+// marvel_heros.push(dc_heros);
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[4][1]);
+
+const all_heros = marvel_heros.concat(dc_heros);
+console.log(all_heros);
+
+
