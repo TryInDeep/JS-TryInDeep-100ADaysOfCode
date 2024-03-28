d1ace036d769051d6102425bf9586d474012439f
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2024-03-28 16:33:34 +0000

    learn some new methods

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -4,8 +4,167 @@
 // [] - brackets
 
 
-let myArr = [0 ,1 , 2, 3, 4, 5] // true, "Tridip"] // it can be different
-console.log(myArr[4]);
+const myArr = [0 ,1 , 2, 3, 4, 5] // true, "Tridip"] // it can be different
+const myHeros =["IronMan","Captain America","Thor"]
 
+
+const myArr2 =  new Array(1 ,2 ,3 ,4);
+// console.log(myArr[1]);
+
+//Methods
+// myArr.push(6);
+// console.log(myArr);
+// myArr.pop();
+
+//myArr.unshift(9);
+// myArr.shift();
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(typeof newArr);
+
+
+//slice , splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3) // first and before last 
+
+console.log(myn1);
+
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C",myArr);
+console.log(myn2);
+
+// at
+// : 
+// ƒ at()
+// concat
+// : 
+// ƒ concat()
+// constructor
+// : 
+// ƒ Array()
+// copyWithin
+// : 
+// ƒ copyWithin()
+// entries
+// : 
+// ƒ entries()
+// every
+// : 
+// ƒ every()
+// fill
+// : 
+// ƒ fill()
+// filter
+// : 
+// ƒ filter()
+// find
+// : 
+// ƒ find()
+// findIndex
+// : 
+// ƒ findIndex()
+// findLast
+// : 
+// ƒ findLast()
+// findLastIndex
+// : 
+// ƒ findLastIndex()
+// flat
+// : 
+// ƒ flat()
+// flatMap
+// : 
+// ƒ flatMap()
+// forEach
+// : 
+// ƒ forEach()
+// includes
+// : 
+// ƒ includes()
+// indexOf
+// : 
+// ƒ indexOf()
+// join
+// : 
+// ƒ join()
+// keys
+// : 
+// ƒ keys()
+// lastIndexOf
+// : 
+// ƒ lastIndexOf()
+// length
+// : 
+// 0
+// map
+// : 
+// ƒ map()
+// pop
+// : 
+// ƒ pop()
+// push
+// : 
+// ƒ push()
+// reduce
+// : 
+// ƒ reduce()
+// reduceRight
+// : 
+// ƒ reduceRight()
+// reverse
+// : 
+// ƒ reverse()
+// shift
+// : 
+// ƒ shift()
+// slice
+// : 
+// ƒ slice()
+// some
+// : 
+// ƒ some()
+// sort
+// : 
+// ƒ sort()
+// splice
+// : 
+// ƒ splice()
+// toLocaleString
+// : 
+// ƒ toLocaleString()
+// toReversed
+// : 
+// ƒ toReversed()
+// toSorted
+// : 
+// ƒ toSorted()
+// toSpliced
+// : 
+// ƒ toSpliced()
+// toString
+// : 
+// ƒ toString()
+// unshift
+// : 
+// ƒ unshift()
+// values
+// : 
+// ƒ values()
+// with
+// : 
+// ƒ with()
+// Symbol(Symbol.iterator)
+// : 
+// ƒ values()
 
 
