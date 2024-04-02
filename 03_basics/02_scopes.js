a41d56723f3b7aafaed076eff43bfd76a286b6fb
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2024-04-02 16:34:06 +0000

    scope level

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,13 +9,30 @@ if(true)
     //block/ local scope
     let a = 10;
     const b = 20;
-    console.log("INNER : ",a);
+    // console.log("INNER : ",a);
     //  c = 30; 
 }
 
 // console.log(a);
 // console.log(b);
-console.log(a);
+// console.log(a);
 
 
 // in website console and code environment scpopes are different 
+
+
+
+
+function one() {
+    const username = "Tridip"
+
+    function two(){
+        const website =  "Youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    // two()
+}
+
+one()
