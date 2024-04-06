b97419ca2552b727a48ef967bace85a400aa0301
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2024-04-06 17:38:34 +0000

    if esle

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,54 @@
+
+//if
+// const isUserLoggedIn = true;
+// const temperature = 41;
+// if(temperature < 50)//true
+// {
+//     console.log("less than 50");
+// }else
+// {
+//     console.log("Temareture is greater than 50");
+// }
+
+// console.log("Executed");
+// < , > , <= , >= ,!= , == , ===
+
+// var scope is global 
+// const score = 200
+
+// if(score > 100)
+// {
+//     const power = "Fly";
+//     console.log(`User Power : ${power} `);
+// }
+// console.log(`User Power : ${power} `);
+
+
+const balance = 1000;
+
+// if (balance > 500) console.log("Test"),console.log("test2"); // need to give ; implicit Scope 
+// we dont recomonded
+
+if(balance < 500)
+{
+    console.log("Less than 500");
+}else if(balance < 750)
+{
+    console.log("Less than 750");
+
+}else if(balance < 900){
+    console.log("Less than 900");
+}else{
+    console.log("less than 1200");
+}
+
+
+
+
+
+
+
+
+
+
+
