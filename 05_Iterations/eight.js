76175b7c99e83651e5bec031bc4c9c5b5b46005a
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2024-04-14 18:04:14 +0000

    Reduce in javascript

--- 05_Iterations/eight.js
+++ 05_Iterations/eight.js
@@ -12,9 +12,37 @@
 
 
 const myNums = [1,2,3]
-const myTotal = myNums.reduce(function (acc,curval) {
-    console.log(`acc : ${acc} and curval : ${curval}`);
-    return acc + curval
-},3) // Initialvalue 
+// const myTotal = myNums.reduce(function (acc,curval) {
+//     console.log(`acc : ${acc} and curval : ${curval}`);
+//     return acc + curval
+// },3) // Initialvalue 
+
+
+const myTotal = myNums.reduce((acc, curr) =>  acc + curr, 0 )
 
 console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName:"JS Course",
+        price: 2999
+    },
+    {
+        itemName:"Java Course",
+        price: 1999
+    },
+    {
+        itemName:"C++ Course",
+        price: 999
+    },
+    {
+        itemName:"Python Course",
+        price: 3999
+    }
+
+]
+
+const priceToPay = shoppingCart.reduce((acc, item)=> acc + item.price, 0)
+
+console.log(priceToPay);
